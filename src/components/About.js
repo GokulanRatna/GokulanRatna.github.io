879d92af46d719c28aaaeac700722d0419af8b4b
Gokulan Vigneswaran <gokulanvigneswaran@Gokulans-MacBook-Air.local>
2024-04-17 17:41:45 +0100

    Prepare for deployment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const About = () => {
+  React.useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
     <div>
       <Navigation />
@@ -10,23 +16,28 @@ const About = () => {
         <div className="row justify-content-center">
           <div className="col-12 col-md-8">
             <h1 className="mb-4">About Me</h1>
-            <img 
-              src={`${process.env.PUBLIC_URL}/assets/images/myphoto.jpeg`} 
-              alt="Gokulan Vigneswaran" 
-              className="img-fluid rounded-circle mb-4" 
-              style={{ maxWidth: '200px', height: 'auto' }} 
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/myphoto.jpeg`}
+              alt="Gokulan Vigneswaran"
+              className="img-fluid rounded-circle mb-4"
+              style={{ maxWidth: '200px', height: 'auto', display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+              data-aos="fade-up"
             />
-            <p>Hello, I'm Gokulan Vigneswaran, a recent graduate from Birmingham City University with a BSc (Hons) in Computer and Data Science, where I specialised in machine learning and deep learning, crafting models for various analytical tasks. My passion lies in leveraging technology to solve real-world problems, and I have a proven track record of delivering high-quality, data-driven solutions.</p>
-
-            <p>My technical toolkit is diverse, encompassing advanced proficiency in Java and Python, alongside strong skills in JavaScript, PHP, HTML, and CSS. I have extensive experience with SQL and NoSQL databases, as well as data visualisation tools such as R, Power BI, and Tableau. My interest in machine learning and deep learning is complemented by hands-on experience with TensorFlow, Keras, and scikit-learn, enabling me to apply these techniques to practical challenges.</p>
-
-            <p>Professionally, I've contributed to various sectors, including a significant role as a Production Supervisor at Do & Co Airline Catering and an IT Intern at ACCMA Accountants. These roles have honed my abilities in project management, team leadership, and the practical application of my technical skills in a business context.</p>
-
-            <p>Outside of my professional life, my interests are broad and varied, including technology, physics, Brazilian Jiu-Jitsu, bodybuilding, Formula 1 motorsports, aerospace, and cars. I also enjoy exploring books on philosophy, finance, geopolitics, and history to expand my knowledge and understanding of the world.</p>
-
-            <p>I am a native English speaker with Tamil as my mother tongue, and I possess a basic understanding of Spanish, reflecting my multicultural background and ability to adapt to diverse environments.</p>
-
-            <p>Through my portfolio, I aim to showcase my journey, the projects I've been part of, and the impact of my work. I'm excited to explore opportunities where I can contribute to impactful projects and drive growth. Connect with me on <a href="https://linkedin.com/in/GokulanVigneswaran" className="text-primary">LinkedIn</a> or check out my work on <a href="https://github.com/GokulanRatna?tab=repositories" className="text-primary">GitHub</a>.</p>
+            <div data-aos="fade-right">
+              <p>Hello, I'm Gokulan Vigneswaran, a recent graduate from Birmingham City University with a BSc (Hons) in Computer and Data Science, specializing in machine learning and deep learning.</p>
+            </div>
+            <div data-aos="fade-left">
+              <p>My technical toolkit is diverse, encompassing advanced proficiency in Java and Python, alongside strong skills in JavaScript, PHP, HTML, and CSS.</p>
+            </div>
+            <div data-aos="fade-right">
+              <p>Professionally, I've contributed to various sectors, including a significant role as a Production Supervisor at Do & Co Airline Catering and an IT Intern at ACCMA Accountants.</p>
+            </div>
+            <div data-aos="fade-left">
+              <p>Outside of my professional life, my interests are broad and varied, including technology, physics, Brazilian Jiu-Jitsu, bodybuilding, Formula 1 motorsports, aerospace, and cars.</p>
+            </div>
+            <div data-aos="fade-up">
+              <p>Through my portfolio, I aim to showcase my journey, the projects I've been part of, and the impact of my work. Connect with me on <a href="https://linkedin.com/in/GokulanVigneswaran" className="text-primary">LinkedIn</a> or check out my work on <a href="https://github.com/GokulanRatna?tab=repositories" className="text-primary">GitHub</a>.</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,74 @@
-import React from 'react';
-import Navigation from './Navigation'; // Ensure the path is correct
-
-const homeStyle = {
-  textAlign: 'center',
-  padding: '50px 20px',
-  background: '#f0f0f0', // A light grey background for a subtle contrast
-};
-
-const headerStyle = {
-  fontSize: '2.5rem',
-  color: '#282c34', // Keeping the main header dark for contrast
-};
-
-const textStyle = {
-  fontSize: '1.2rem',
-  color: '#666', // A softer color for the text for readability
-};
-
-const buttonStyle = {
-  marginTop: '20px',
-  padding: '10px 20px',
-  fontSize: '1rem',
-  backgroundColor: '#4CAF50', // A vibrant color for the Call To Action (CTA) button
-  color: 'white',
-  border: 'none',
-  borderRadius: '5px',
-  cursor: 'pointer',
-};
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import Navigation from './Navigation';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  const navigate = useNavigate(); // Create navigate function object
+
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Animation duration
+      once: true, // Animation occurs only once during scroll
+    });
+  }, []);
+
+  const navigateToProjects = () => {
+    navigate('/projects'); // Use React Router's navigate to go to projects
+  };
+
   return (
     <div>
       <Navigation />
-      <div style={homeStyle}>
-        <h1 style={headerStyle}>Welcome to My Digital World</h1>
-        <p style={textStyle}>
+      <div style={{
+        textAlign: 'center',
+        padding: '50px 20px',
+        background: '#f0f0f0',
+      }}>
+        <h1 style={{
+          fontSize: '2.5rem',
+          color: '#282c34',
+          marginBottom: '20px',
+        }} data-aos="fade-down">
+          Welcome to My Digital World
+        </h1>
+        <div style={{
+          width: '300px',
+          height: '300px',
+          overflow: 'hidden',
+          margin: '0 auto',
+          borderRadius: '55%'
+        }}>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/earth.gif`}
+            alt="Spinning Earth"
+            className="img-fluid"
+            style={{
+              width: 'auto',
+              height: '100%',
+              marginLeft: '0%'
+            }}
+            data-aos="zoom-in"
+          />
+        </div>
+        <p style={{
+          fontSize: '1.2rem',
+          color: '#666',
+          marginTop: '20px',
+        }} data-aos="fade-up">
           I'm Gokulan Vigneswaran, a Computer and Data Science enthusiast with a passion for technology, innovation, and continuous learning. Dive into my portfolio to discover projects that not only demonstrate my technical skills but also my journey towards making a significant impact in the tech community.
         </p>
-        <button style={buttonStyle} onClick={() => window.location.href = '/projects'}>
+        <button style={{
+          marginTop: '20px',
+          padding: '10px 20px',
+          fontSize: '1rem',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+        }} onClick={navigateToProjects} data-aos="fade-up">
           Explore My Projects
         </button>
       </div>
